768be4ab5297e9e8b94f7bf9d9ebc42d58ece123
MaViniciusDev <marcelovldgomes@outlook.com>
2025-03-09 15:44:33 -0300

    Criação do projeto e adição das dependências essenciais

---
+++ src/test/java/com/ozzmakappa/scea/SceaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozzmakappa.scea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SceaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
